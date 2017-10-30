b8f765e018136f954a7b74fdb5fb8e18f38d6b03
Robert Pergl <robert@pergl.org>
2017-10-30 20:28:42 +0100

    deploy script fixes

--- Server/bin/deploy.sh
+++ Server/bin/deploy.sh
@@ -6,8 +6,8 @@ D="~/rsync/$S"
 
 ssh -t rob@ccmi.fit.cvut.cz "tar cfzv $S.tar.gz $D"
 
-stack install $S
+stack install
 rsync -rvz --delete -e ssh bin static rob@ccmi.fit.cvut.cz:$D
-rsync -rvz --delete -e ssh ~/.local/bin/$F rob@ccmi.fit.cvut.cz:$D/$S
-
-ssh -t rob@ccmi.fit.cvut.cz "sudo systemctl restart $S.service"
+ssh -t rob@ccmi.fit.cvut.cz "sudo systemctl stop $S.service"
+scp ~/.local/bin/$F rob@ccmi.fit.cvut.cz:$D/$S
+ssh -t rob@ccmi.fit.cvut.cz "sudo systemctl start $S.service"
